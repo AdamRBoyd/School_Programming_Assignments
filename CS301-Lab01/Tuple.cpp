f6bc76ead04c592f58a17ddb253781be454693bb
Adam Boyd <aboyd08@gmail.com>
2020-01-30 14:15:56 -0800

    Done?

--- CS301-Lab01/Tuple.cpp
+++ CS301-Lab01/Tuple.cpp
@@ -21,18 +21,17 @@ void Tuple::set(int position, int value) {
 		cout << "Invalid Position - " << position << " Not in Range!;
 		return;
 	}
-	if (value < MINVALUE || value > MAXVALUE) {
-		cout << "Invalid Value - " << value << " Not in Range!!";
-		return;
-	}
+	if (value < MINVALUE) value = MINVALUE;
+	if (value > MAXVALUE) value = MAXVALUE;
+
 	items[position-1] = value;
 }
 
 
-void Tuple::rotate(int amount) {  //<----------Finish!!
+void Tuple::rotate(int amount) {
+	if (amount < 0 || amount % SIZE = 0) return;
 	amount %= SIZE;
-	if (amount == 0) return;
-
+	
 	int temp[SIZE];
 
 	//create temporary array to hold original values
@@ -60,34 +59,80 @@ void Tuple::print() const {
 	cout << "{";
 	for (int i = 0; i < SIZE; i++) {
 		cout << items[i];
-		if (i < SIZE - 1) cout << ", ";
+		if (i < SIZE - 1) cout << ",";
 	}
+	cout << "}\n";
 }
 
 double Tuple::getAverage() const {
-
-
-
+	int total = 0;
+	for (int i = 0; i < SIZE; i++) {
+		total += items[i];
+	}
+	total /= SIZE;
+	return total;
 }
 
-
 void Tuple::sort() {
-
-
-
-
+	bool FLAG = true;
+	for (int i = 0; i <= SIZE && FLAG; i++) {
+		FLAG = false;
+		for (int j = 0; j < Size - 1; j++) {
+			if (items[i + 1] > items[i]) {
+				int temp = items[i];
+				items[i] = items[i + 1];
+				items[i + 1] = temp;
+				FLAG = true;
+			}
+		}
+	}
 }
 
 
 bool Tuple::equal(const Tuple& t) const {
-
-
-
+	for (int i = 0; i < SIZE; i++) {
+		if (t.items[i] != items[i]) return false;	
+	}
+	return true;
 }
 
 
 bool Tuple::equalBags(const Tuple& t) const {
+	int temp1[SIZE];
+	int temp2[SIZE];
 
+	copy(begin(items), end(items), begin(temp1));
+	copy(begin(t.items), end(t.items), begin(temp2));
+	
+	bool FLAG = true;
+	for (int i = 0; i <= SIZE && FLAG; i++) {
+		FLAG = false;
+		for (int j = 0; j < Size - 1; j++) {
+			if (temp1[i + 1] > temp1[i]) {
+				int temp = temp1[i];
+				temp1[i] = temp1[i + 1];
+				temp1[i + 1] = temp;
+				FLAG = true;
+			}
+		}
+	}
+	
+	FLAG = true;
+	for (int i = 0; i <= SIZE && FLAG; i++) {
+		FLAG = false;
+		for (int j = 0; j < Size - 1; j++) {
+			if (temp2[i + 1] > temp2[i]) {
+				int temp = temp2[i];
+				temp2[i] = temp2[i + 1];
+				temp2[i + 1] = temp;
+				FLAG = true;
+			}
+		}
+	}
 
+	for (int i = 0; i < SIZE; i++) {
+		if (temp1[i] != temp2[i]) return false;
+	}
 
+	return true;
 }
