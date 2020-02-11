193ba78c480dfd3a59e441a59573830f271265d2
Adam Boyd <aboyd08@gmail.com>
2020-02-11 14:28:21 -0800

    CS301 Lab 3

--- CS301-Lab03/CS301-Lab03.cpp
+++ CS301-Lab03/CS301-Lab03.cpp
@@ -1,18 +1,3 @@
-// CS301-Lab03.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
-/*Implement a LinkedList class that uses a doubly-linked list as the underlying data structure. 
-That means each node will have both a previous and a next pointer along with the data.   
-This will be a class template similar to the LinkedList code I posted on Blackboard except we will include 
-fields int length; and Node * tailPtr;  along with the original Node * headPtr.
-
-Implement the same functions as in the posted LInkedList class, along with corresponding insertLast, removeLast and getLast methods.  
-Also provide a getLength() method and a reverse method that reverses the order of element in the list. 
-Your reverse method should be code efficiently, meaning don't allocate any new nodes or move data between node. 
-Just update the pointers, if possible.
-
-Upload your LinkedList.h file and a tester.cpp that does some tests on LinkedList object. 
-You're graded on the correctness of LinkedList.h not on the quality of your tests.*/
-
 #include "LinkedList.h"
 #include <iostream>
 
@@ -21,41 +6,36 @@ using namespace std;
 int main()
 {
     LinkedList<int> myList;
+    cout << "initial list" << endl;
     cout << myList << endl;
+    cout << "Length: " << myList.getLength() << endl << endl;
+    cout << "insertFirst 5" << endl;
     myList.insertFirst(5);
+    cout << "insertFirst 6" << endl;
     myList.insertFirst(6);
+    cout << "insertFirst 7" << endl;
     myList.insertFirst(7);
-    cout << myList << endl;
+    cout << "mylist" << endl;
+    cout << myList << endl << endl;
+    cout << "copy list" << endl;
     LinkedList<int> yourList = myList; // same as yourList(myList)
+    cout << "setFirst 100" << endl;
     yourList.setFirst(100);
+    cout << "yourlist" << endl;
     cout << yourList << endl;
+    cout << "mylist" << endl;
     cout << myList << endl;
-
-    /*
-    myList.removeFirst();
+    cout << "end" << endl;
+    
+    cout << "Length: " << myList.getLength() << endl;
+    cout << "insertLast" << endl;
+    myList.insertLast(9);
     cout << myList << endl;
-    cout << myList.getFirst() << endl;
-    cout << myList.getLast() << endl;
-
-    myList.removeFirst();
-
+    cout << "Length: " << myList.getLength() << endl;
+    cout << "removeLast" << endl;
+    myList.removeLast();
     cout << myList << endl;
-    cout << myList.getFirst() << endl;
-    cout << myList.getLast() << endl;
-
-    myList.removeFirst();
-
-    cout << myList << endl;
-
-    try {
-            cout << myList.getFirst() << endl;
-            cout << "done" << endl;
-    }
-
-    catch (logic_error e) {
-            cout << e.what() << endl;
-    }
-    */
+    cout << "Length: " << myList.getLength() << endl;
 
     return 0;
 }
--- CS301-Lab03/LinkedList.h
+++ CS301-Lab03/LinkedList.h
@@ -1,3 +1,18 @@
+// CS301-Lab03.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+/*Implement a LinkedList class that uses a doubly-linked list as the underlying data structure.
+That means each node will have both a previous and a next pointer along with the data.
+This will be a class template similar to the LinkedList code I posted on Blackboard except we will include
+fields int length; and Node * tailPtr;  along with the original Node * headPtr.
+
+Implement the same functions as in the posted LInkedList class, along with corresponding insertLast, removeLast and getLast methods.
+Also provide a getLength() method and a reverse method that reverses the order of element in the list.
+Your reverse method should be code efficiently, meaning don't allocate any new nodes or move data between node.
+Just update the pointers, if possible.
+
+Upload your LinkedList.h file and a tester.cpp that does some tests on LinkedList object.
+You're graded on the correctness of LinkedList.h not on the quality of your tests.*/
+
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 #include <iostream>
@@ -9,15 +24,14 @@ class Node {
 public:
     T data;
     Node<T>* next;
+    Node<T>* prev;
 
-    Node(const T& d = T(), Node<T>* n = nullptr) {
+    Node(const T& d = T(), Node<T>* n = nullptr, Node<T>* p = nullptr) {
         data = d;
         next = n;
+        prev = p;
     }
 };
-
-
-
 
 template <class T>
 class LinkedList {
@@ -62,16 +76,16 @@ public:
     }
 
     void insertFirst(const T& d) {
-        Node<T>* toInsert = new Node<T>(d, headPtr);
+        Node<T>* toInsert = new Node<T>(d, headPtr, nullptr);
         headPtr = toInsert;
     }
     void removeFirst() {
-        //if (headPtr == nullptr)
-        //      throw logic_error("Can't remove from an empty list.");
         if (headPtr == nullptr)
             return;
         Node<T>* temp = headPtr;
         headPtr = headPtr->next;
+        if(headPtr != nullptr && headPtr->next != nullptr)
+            headPtr->next->prev = nullptr;
         delete temp;
     }
     T getFirst() const {
@@ -79,6 +93,30 @@ public:
             throw logic_error("Can't get first for an empty list.");
         return headPtr->data;
     }
+
+    //insertLast, removeLast
+    void insertLast(const T& d) {
+        Node<T>* current = headPtr;
+        if (headPtr != nullptr) {
+            while (current->next != nullptr)
+                current = current->next;
+        }
+        Node<T>* toInsert = new Node<T>(d, nullptr, current);
+        current->next = toInsert;
+    }
+
+    void removeLast() {
+        if (headPtr == nullptr)
+            throw logic_error("Can't remove last for an empty list.");
+        Node<T>* current = headPtr;
+        while (current->next != nullptr)
+            current = current->next;
+
+        Node<T>* temp = current;
+        current->prev->next = nullptr;
+        delete temp;
+    }
+
     T getLast() const {
         if (headPtr == nullptr)
             throw logic_error("Can't get last for an empty list.");
@@ -102,6 +140,21 @@ public:
             removeFirst();
     }
 
+    int getLength() {
+        if (headPtr == nullptr)
+            return 0;
+        Node<T>* current = headPtr;
+        int count = 1;
+        while (current->next != nullptr) {
+            current = current->next;
+            count++;
+        }
+        return count;
+    }
+//reverse method
+    void reverse() {
+
+    }
 
 };
 
