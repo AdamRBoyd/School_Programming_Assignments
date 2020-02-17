b9046e9bb9301ed159a354093cd06452e5cb0d4d
Adam Boyd <aboyd08@gmail.com>
2020-02-17 01:12:36 -0800

    Doneish

--- CS311-HW1/CS311-HW1.c
+++ CS311-HW1/CS311-HW1.c
@@ -1,20 +1,6 @@
-/*#include <iostream>
-#include <string>
-#include <cstdint>
-#include <vector>
-using namespace std;*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-/*enum LetterGrade {
-	A = 4,
-	B = 3,
-	C = 2,
-	D = 1,
-	F = 0
-};*/
 
 enum LetterGrade {
 	A = 4,
@@ -24,17 +10,6 @@ enum LetterGrade {
 	F = 0
 };
 
-// type T must be castable into a double
-/*template<class T>
-double getArrayAverage(vector<T>& vec) {
-	double sum = 0;
-	for (const auto& value : vec) {
-		sum += static_cast<double>(value);
-	}
-	const auto avg = sum / vec.size();
-	return avg;
-}*/
-
 double getArrayAverage(int arr[], int size) {
 	double sum = 0;
 	for (int i = 0; i < size; ++i) {
@@ -43,29 +18,6 @@ double getArrayAverage(int arr[], int size) {
 	
 	return sum / size;
 }
-
-/*void convertCharToLetterGrade(char& grade) {
-	switch (grade) {
-	case 'A': case 'a':
-		grade = 4;
-		return;
-	case 'B': case 'b':
-		grade = 3;
-		return;
-	case 'C': case 'c':
-		grade = 2;
-		return;
-	case 'D': case 'd':
-		grade = 1;
-		return;
-	case 'F': case 'f':
-		grade = 0;
-		return;
-	default:
-		cout << "Warning... Invalid Character... Recording an F.\n";
-		return;
-	}
-}*/
 
 void convertCharToLetterGrade(char *grade) {
 	switch (*grade) {
@@ -91,19 +43,6 @@ void convertCharToLetterGrade(char *grade) {
 	}
 }
 
-/*LetterGrade getLetterGradeFromAverage(const double avg) {
-	if (avg >= 90)
-		return LetterGrade::A;
-	else if (avg >= 80)
-		return LetterGrade::B;
-	else if (avg >= 70)
-		return LetterGrade::C;
-	else if (avg >= 60)
-		return LetterGrade::D;
-	else
-		return LetterGrade::F;
-}*/
-
 enum LetterGrade getLetterGradeFromAverage(const double avg) {
 	enum LetteGrade grade;
 	if (avg >= 90)
@@ -120,25 +59,13 @@ enum LetterGrade getLetterGradeFromAverage(const double avg) {
 }
 
 int main() {
-	/*string firstName;
-	cout << "Please enter your first name: ";
-	cin >> firstName;
-	string lastName;
-	cout << "Please enter your last name: ";
-	cin >> lastName;
-	int32_t numPrevCourses;
-	cout << "Enter number of previous courses: ";
-	cin >> numPrevCourses;
-	cin.ignore();*/
+	char c;
 	
-	char c;  //used for input flush
-	//char* firstName, * lastName, * fullName;
-	
-	char firstName[50];							//<<<<<<<<<<<<<<<<<<<<<<<<<<MAKE DYNAMIC<<<<<<<<<<<<<<<<<<<<<<<<<<<< <
+	char firstName[50];
 	printf("Please enter your first name: ");
 	scanf("%s", &firstName);
 
-	char lastName[50];							//<<<<<<<<<<<<<<<<<<<<<<<<<<MAKE DYNAMIC<<<<<<<<<<<<<<<<<<<<<<<<<<<< <
+	char lastName[50];
 	printf("Please enter your last name: ");
 	scanf("%s", &lastName);
 
@@ -146,19 +73,7 @@ int main() {
 	printf("\nEnter number of previous courses: ");
 	scanf("%d", &numPrevCourses);
 	
-	//flush input buffer aka cin.ignore()
 	while ((c = getchar()) != '\n' && c != EOF) {}
-
-	/*vector<LetterGrade> prevGrades(numPrevCourses);
-
-	for (int32_t courseIx = 0; courseIx < numPrevCourses; ++courseIx) {
-		cout << "Enter letter grade for course " << courseIx << ": ";
-		char letterGrade;
-		cin.get(letterGrade);
-		cin.ignore();
-		convertCharToLetterGrade(letterGrade);
-		prevGrades.at(courseIx) = static_cast<LetterGrade>(letterGrade);
-	}*/
 
 	enum LetterGrade* prevGrades = (enum LetterGrade*)malloc(numPrevCourses * sizeof(enum LetterGrade));
 
@@ -166,7 +81,7 @@ int main() {
 		printf("Enter letter grade for course %d: ", courseIx + 1);
 		char letterGrade;
 		scanf("%c", &letterGrade);
-		//flush input buffer aka cin.ignore()
+		
 		while ((c = getchar()) != '\n' && c != EOF) {}
 
 		convertCharToLetterGrade(&letterGrade);
@@ -174,23 +89,10 @@ int main() {
 		prevGrades[courseIx] = letterGrade;
 	}
 	
-	/*int32_t numExams;
-	cout << "Enter number of exams this semester: ";
-	cin >> numExams;
-	cin.ignore();
-
-	vector<int32_t> examGrades(numExams);
-
-	for (int32_t examIx = 0; examIx < numExams; ++examIx) {
-		cout << "Enter grade for exam " << examIx << " as an integer: ";
-		cin >> examGrades.at(examIx);
-		cin.ignore();
-	}*/
-
 	int numExams;
 	printf("\nEnter number of exams this semester: ");
 	scanf("%d", &numExams);
-	//flush input buffer aka cin.ignore()
+	
 	while ((c = getchar()) != '\n' && c != EOF) {}
 
 	int* examGrades = (int*)malloc(numExams * sizeof(int));
@@ -198,14 +100,11 @@ int main() {
 	for(int examIx = 0; examIx < numExams; ++examIx) {
 		printf("%s %d %s", "Enter grade for exam ", examIx+1, " as an integer: ");
 		scanf("%d", &examGrades[examIx]);
-		//flush input buffer aka cin.ignore()
+		
 		while ((c = getchar()) != '\n' && c != EOF) {}
 	}
 
-	/*const auto fullName = firstName + " " + lastName;
-	cout << "Grade Report For " << fullName << ":\n";*/
-
-	char fullname[100];										//<<<<<<<<<<<<<<<<<<<<<<<<<<MAKE DYNAMIC<<<<<<<<<<<<<<<<<<<<<<<<<<<< <
+	char fullname[100];
 	int nameCat = 0;
 
 	for (; firstName[nameCat] != '\0'; nameCat++) {
@@ -220,23 +119,13 @@ int main() {
 
 	printf("%s %s:\n", "\nGrade Report for", fullname);
 
-	/*const auto examAverage = getArrayAverage(examGrades);
-	cout << "Your exam average is: " << examAverage << "\n";*/
-
 	double examAverage = getArrayAverage(examGrades, numExams);
 	printf("%s %.2f\n", "Your exam average is:", examAverage);
-
-	// get GPA with newest course added:
-	/*const auto newLetterGrade = getLetterGradeFromAverage(examAverage);
-	prevGrades.push_back(newLetterGrade);*/
 
 	enum LetterGrade newLetterGrade = getLetterGradeFromAverage(examAverage);
 
 	prevGrades = realloc(prevGrades, ++numPrevCourses * sizeof(enum LetterGrade));
 	prevGrades[numPrevCourses-1] = newLetterGrade;
-
-	/*const auto gpa = getArrayAverage(prevGrades);
-	cout << "Your latest GPA is: " << gpa << "\n";*/
 
 	double gpa = getArrayAverage(prevGrades, numPrevCourses);
 	printf("%s %.2f\n", "Your latest GPA is:", gpa);
