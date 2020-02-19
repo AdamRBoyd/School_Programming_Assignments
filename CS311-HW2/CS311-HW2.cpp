bac9072d0c7a452aee554f4ea16b9a98ae6c7042
Adam Boyd <aboyd08@gmail.com>
2020-02-19 00:51:53 -0800

    random not working right...

--- CS311-HW2/CS311-HW2.cpp
+++ CS311-HW2/CS311-HW2.cpp
@@ -6,21 +6,109 @@
 #include <vector>
 #include <unordered_map>
 #include <fstream>
-#include <utility>
+#include <string>
 #include "Production.h"
 
 using namespace std;
 
-bool findAndIdentify(const string inStr, string &ostr, int &b, int &e) {
-
+bool findAndIdentify(const string inStr, size_t &beg, size_t &end) {
+	beg = inStr.find("<");
+	end = inStr.find(">");
+	if (beg != string::npos) {
+		return true;
+	}
+	return false;
 }
 
 int main() {
-	int mainStr, keyStr;
-	int beg, end;
-	unordered_map<string, Production> myMap;
+	string keyStr, newStr, mainStr = "int main() {\n <stat_list> \nreturn 0; \n}";
+	size_t beg, end;
+	
+	vector<vector<string>> vecStr{ 
+		/*<stat_list>*/{"<stat>", "<stat_list> <stat>"}, 
+		/*<stat>*/{"<cmpd_stat>", "<if_stat>", "<iter_stat>", "<assgn_stat>", "<decl_stat>"}, 
+		/*<cmpd_stat>*/{"{\n <stat_list> \n}"}, 
+		/*<if_stat>*/{"if ( <exp> ) <stat>", "if ( <exp> ) <cmpd_stat>", "if ( <exp> ) <stat> \nelse <stat>", "if ( <exp> ) <cmpd_stat> \nelse <stat>", "if ( <exp> ) <stat> \nelse <cmpd_stat>", "if ( <exp> ) <cmpd_stat> \nelse <cmpd_stat>"}, 
+		/*<iter_stat>*/{"while ( <exp> ) <stat>", "while ( <exp> ) <cmpd_stat>"}, 
+		/*<assgn_stat>*/{"<id> = <exp>"}, 
+		/*<decl_stat>*/{"<type> <id>", "<type> <assgn_stat>"}, 
+		/*<exp>*/{"<exp> <op> <exp>", "<id>", "<const>"}, 
+		/*<op>*/{"+ ","- ","* ","/ "}, 
+		/*<type>*/{"int ", "double "}, 
+		/*<id>*/{"<char><char_digit_seq>"}, 
+		/*<const>*/{"<digit><digit_seq>"}, 
+		/*<char_digit_seq>*/{" ", "<char><char_digit_seq>", "<digit><char_digit_seq>"}, 
+		/*<digit_seq>*/{" ", "<digit><digit_seq>"}, 
+		/*<char>*/{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"},
+		/*<digit>*/{"0","1","2","3","4","5","6","7","8","9"}
+	};
+	vector<vector<double>> vecDoub{
+		/*<stat_list>*/{.9, .1},
+		/*<stat>*/{0.05, 0.05, 0.05, 0.5, 0.35},
+		/*<cmpd_stat>*/{1},
+		/*<if_stat>*/{ 1, 0.15, 0.15, 0.15, 0.2, 0.2},
+		/*<iter_stat>*/{ .75, 0.25},
+		/*<assgn_stat>*/{1},
+		/*<decl_stat>*/{1, 0.5},
+		/*<exp>*/{1, 0.3, 0.5},
+		/*<op>*/{1},
+		/*<type>*/{1},
+		/*<id>*/{1},
+		/*<const>*/{1},
+		/*<char_digit_seq>*/{1, 0.3, 0.45},
+		/*<digit_seq>*/{1, 0.5},
+		/*<char>*/{1},
+		/*<digit>*/{1}
+	};
+
+	Production	stat_list(vecStr[0], vecDoub[0]),
+				stat(vecStr[1], vecDoub[1]),
+				cmpd_stat(vecStr[2], vecDoub[2]),
+				if_stat(vecStr[3], vecDoub[3]),
+				iter_stat(vecStr[4], vecDoub[4]),
+				assgn_stat(vecStr[5], vecDoub[5]),
+				decl_stat(vecStr[6], vecDoub[6]),
+				exp(vecStr[7], vecDoub[7]),
+				op(vecStr[8], vecDoub[8]),
+				type(vecStr[9], vecDoub[9]),
+				id(vecStr[10], vecDoub[10]),
+				consta(vecStr[11], vecDoub[11]),
+				char_digit_seq(vecStr[12], vecDoub[12]),
+				digit_seq(vecStr[13], vecDoub[13]),
+				chara(vecStr[14], vecDoub[14]),
+				digit(vecStr[15], vecDoub[15]);
+
+	unordered_map<string, Production> myMap = { 
+		{"<stat_list>", stat_list}, 
+		{"<stat>", stat}, 
+		{"<cmpd_stat>", cmpd_stat}, 
+		{"<if_stat>", if_stat},	
+		{"<iter_stat>", iter_stat},
+		{"<assgn_stat>", assgn_stat},
+		{"<decl_stat>", decl_stat},
+		{"<exp>", exp},
+		{"<op>", op},
+		{"<type>", type},
+		{"<id>", id},
+		{"<const>", consta},
+		{"<char_digit_seq>", char_digit_seq},
+		{"<digit_seq>", digit_seq},
+		{"<char>", chara},
+		{"<digit>", digit}
+	};
 
 
+	while (findAndIdentify(mainStr, beg, end)) {
+		size_t len = end - beg + 1;
+		keyStr = mainStr.substr(beg, len);
+		
+		newStr = myMap.at(keyStr).expand();
+
+		mainStr.replace(beg, len, newStr);
+		cout << mainStr << endl;
+	}
+
+	cout << mainStr << endl;
 
 
 	return 0;
--- CS311-HW2/Production.h
+++ CS311-HW2/Production.h
@@ -2,18 +2,56 @@
 #ifndef PRODUCTION_H
 #define PRODUCTION_H
 
-class Production
-{
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <time.h>
+#include <iterator>
+
+using namespace std;
+
+class Production{
 private:
-	string lhs;
 	vector<string> rhs_options; // list of options for expansion
 	vector<double> trans_probs; // list of probabilities associated with each choice 
 
 public:
-	Production();
-	Production(string s);
-	void add_rhs(string s, double d); // adds new rhs to the production
-	string expand() const; // returns one of the rhs choices using a random number generator
+	Production(const vector<string> vs, const vector<double> vd) {
+		rhs_options = vs;
+
+		if (vd.at(0) == 1) {
+			trans_probs = vd;
+		}
+		else {
+			double cumul = 0;
+			for (int i = 0; i < vd.size(); i++) {
+				cumul += vd.at(i);
+				trans_probs.push_back(cumul);
+			}
+		}
+	}
+
+	string expand() const {		//returns one of the rhs choices using a random number generator
+		srand(time(NULL));
+
+		if (trans_probs[0] != 1) {
+			double random = ((double)rand() / (RAND_MAX));
+
+			auto low = lower_bound(trans_probs.begin(), trans_probs.end(), random);
+			int index = distance(trans_probs.begin(), low);
+			
+			return rhs_options.at(index);
+		}
+
+		int size = rhs_options.size();
+		int random = rand() % size;
+
+		string test = rhs_options.at(random);
+		
+		return test;
+
+	}
 };
 
 #endif // !PRODUCTION_H
