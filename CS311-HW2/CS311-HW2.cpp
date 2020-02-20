132c27f26371f3485709886fbca7fd9bd9da75fc
Adam Boyd <aboyd08@gmail.com>
2020-02-19 20:44:14 -0800

    add <prog>

--- CS311-HW2/CS311-HW2.cpp
+++ CS311-HW2/CS311-HW2.cpp
@@ -23,7 +23,7 @@ bool findAndIdentify(const string inStr, size_t &beg, size_t &end) {
 }
 
 int main() {
-	string keyStr, newStr, mainStr = "int main() {\n <stat_list> \nreturn 0; \n}";
+	string keyStr, newStr, mainStr = "<prog>";
 	size_t beg, end;
 	//ofstream myFile ("OutFile.txt");  //uncomment for file io <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	srand(time(NULL));
@@ -44,7 +44,8 @@ int main() {
 		/*<char_digit_seq>*/{"", "<char><char_digit_seq>", "<digit><char_digit_seq>"}, 
 		/*<digit_seq>*/{"", "<digit><digit_seq>"}, 
 		/*<char>*/{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"},
-		/*<digit>*/{"0","1","2","3","4","5","6","7","8","9"}
+		/*<digit>*/{"0","1","2","3","4","5","6","7","8","9"},
+		/*<prog>*/{"int main() {\n <stat_list> \nreturn 0; \n}"}
 	};
 	vector<vector<double>> vecDoub{				//<<<<<<<<<<<<<< To Do... Set Weights <<<<<<<<<<<<<<<<<<<
 		/*<stat_list>*/{1, .1},
@@ -62,25 +63,27 @@ int main() {
 		/*<char_digit_seq>*/{1, 0.3, 0.45},
 		/*<digit_seq>*/{1, 0.5},
 		/*<char>*/{1},
-		/*<digit>*/{1}
+		/*<digit>*/{1},
+		/*<prog>*/{1},
 	};
 
 	Production	stat_list(vecStr[0], vecDoub[0]),
-				stat(vecStr[1], vecDoub[1]),
-				cmpd_stat(vecStr[2], vecDoub[2]),
-				if_stat(vecStr[3], vecDoub[3]),
-				iter_stat(vecStr[4], vecDoub[4]),
-				assgn_stat(vecStr[5], vecDoub[5]),
-				decl_stat(vecStr[6], vecDoub[6]),
-				exp(vecStr[7], vecDoub[7]),
-				op(vecStr[8], vecDoub[8]),
-				type(vecStr[9], vecDoub[9]),
-				id(vecStr[10], vecDoub[10]),
-				consta(vecStr[11], vecDoub[11]),
-				char_digit_seq(vecStr[12], vecDoub[12]),
-				digit_seq(vecStr[13], vecDoub[13]),
-				chara(vecStr[14], vecDoub[14]),
-				digit(vecStr[15], vecDoub[15]);
+		stat(vecStr[1], vecDoub[1]),
+		cmpd_stat(vecStr[2], vecDoub[2]),
+		if_stat(vecStr[3], vecDoub[3]),
+		iter_stat(vecStr[4], vecDoub[4]),
+		assgn_stat(vecStr[5], vecDoub[5]),
+		decl_stat(vecStr[6], vecDoub[6]),
+		exp(vecStr[7], vecDoub[7]),
+		op(vecStr[8], vecDoub[8]),
+		type(vecStr[9], vecDoub[9]),
+		id(vecStr[10], vecDoub[10]),
+		consta(vecStr[11], vecDoub[11]),
+		char_digit_seq(vecStr[12], vecDoub[12]),
+		digit_seq(vecStr[13], vecDoub[13]),
+		chara(vecStr[14], vecDoub[14]),
+		digit(vecStr[15], vecDoub[15]),
+		prog(vecStr[16], vecDoub[16]);
 
 	unordered_map<string, Production> myMap = { 
 		{"<stat_list>", stat_list}, 
@@ -98,7 +101,8 @@ int main() {
 		{"<char_digit_seq>", char_digit_seq},
 		{"<digit_seq>", digit_seq},
 		{"<char>", chara},
-		{"<digit>", digit}
+		{"<digit>", digit},
+		{"<prog>", prog}
 	};
 
 	cout << "Please Wait.";
