5f805f51722df6314b50ee77b8242f1c1e10fac6
Adam Boyd <aboyd08@gmail.com>
2020-04-04 21:16:57 -0700

    CS311 HW4 creation

---
+++ CS311-HW4/CS311_HW4.py
@@ -0,0 +1,31 @@
+#Investigate the following features or constructs as they pertain to Python:
+
+# 1. interpretation
+# 2. Boolean expressions
+# 3. short circuit evaluation
+# 4. numeric types
+# 5. strings
+# 6. arrays
+# 7. lists
+# 8. tuples
+# 9. slices
+# 10. index range checking
+# 11. dictionaries
+# 12. if statement
+# 13. switch statement
+# 14. for loop
+# 15. while loop
+# 16. indentation to denote code blocks
+# 17. type binding
+# 18. type checking
+# 19. functions
+# 20. one other feature - your choice
+
+#For each of the above features you should do the following:
+
+#   1. Write a short program or programs to investigate its use. You may combine more than one feature into a single program.
+#   2. Explain how the feature works in Python and compare it to one or more languages with which you are familiar.
+#   3. Critique the implementation or use of the feature or construct.
+
+#Note: Some of the features above are not included in Python, in which case you should note that they are missing and critique the decision to leave them out.
+#Finally, you should give an overall critique of Python.
--- PythonTesting/PythonTesting.py
+++ PythonTesting/PythonTesting.py
@@ -1,4 +1,13 @@
-def Hello():
-    print('hello')
-    return 6
+myStr = "Hello, how are you a programmer, and elephant is big, they eat them grass."
+st = {" a ", " an ", " the "}
+t = 0;
 
+for i in st:
+    t = myStr.find(i)
+    if t != -1:
+        break
+
+if t == -1:
+    print("There is not a singular noun")
+else:
+    print("There is a singular noun")
